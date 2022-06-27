2b4894600897386ba418e7170c591c4bcccaeea2
Cyril Jephery <cyriljeph@gmail.com>
2022-06-27 15:36:23 +0100

    first commit

--- logistics/glover.js
+++ logistics/glover.js
@@ -3,3 +3,8 @@ const logo = document.getElementById("glovo-logo");
 logo.onclick = function() {
 alert("Welcome to Glovo!")
 }
+
+// let classred = document.getElementsByClassName('red');
+
+// for(let i = 0; i < classred.length; i++){
+//   classred[i].style.color = "green";}
